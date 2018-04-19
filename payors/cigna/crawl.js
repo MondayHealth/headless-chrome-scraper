1e7c4c586b2f49f136794454410ccb41b5e96cc2
Chris Galardi <christophergalardi@gmail.com>
2018-04-19 15:37:48 -0400

    add cigna crawler

---
+++ payors/cigna/crawl.js
@@ -0,0 +1,9 @@
+export default class Crawl {
+  constructor(browser, redis) {
+    this._browser = browser;
+  }
+
+  async crawl() {
+
+  }
+}
---
+++ payors/cigna/index.js
@@ -0,0 +1,6 @@
+import Crawl from "./crawl";
+
+export async function crawl(browser, redis) {
+  const c = new Crawl(browser, redis);
+  await c.crawl();
+}
